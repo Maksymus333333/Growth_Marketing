11042b54a0f602b3c8a6b46a0811ed0ebe9548c4
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-02-15 22:47:56 +0100

    fix merge

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,11 +1,14 @@
-import React, { JSX } from 'react';
-import { Interests } from '../../modules/Interests';
+import React from 'react';
+
+import { JSX } from 'react';
 import { Header } from '../../modules/Header';
+import { Interests } from '../../modules/Interests';
+
 export const LandingPage = (): JSX.Element => {
   return (
     <div>
       <Header />
-      <Interests />
+      <Interests />;
     </div>
   );
 };

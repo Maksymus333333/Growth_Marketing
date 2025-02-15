673fd8fd43f0b715639f1b627dfc6d26e0ad4726
Maksymus333333 <mdachevskyi@edu.cdv.pl>
2025-02-15 22:36:25 +0100

    fix bags

---
+++ src/Pages/LandingPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { JSX } from 'react';
+import { Header } from '../../modules/Header';
+import { Interests } from '../../modules/Interests';
+
+export const LandingPage = (): JSX.Element => {
+  return (
+    <div>
+      <Header />
+      <Interests />;
+    </div>
+  );
+};

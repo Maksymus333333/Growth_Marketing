02cfe0bed52d49368bcf02b71d6f9b0043a989a4
amslavko <amslavko@gmail.com>
2025-03-26 17:17:03 +0100

    add 404 page +new cookies item

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { JSX } from 'react';
+import './styles/style.css';
+import Image404 from '../../assets/images/page-404.png';
+import { GoMain404 } from './features/GoMain';
+
+
+export const Page404 = (): JSX.Element => {
+  return (
+<section className='page-404-container-wrapper'>
+  <div className='page-404-container'>
+<img src={Image404} className="image-404" alt="Image404" />
+    <div className='text-container-404'>
+    <div className='page-404-text'>
+    <h2 className='h2-404'>Ой! Сталася помилка</h2>
+    <p className='bottom-text-404'>Можливо сторінка була видалена, її назву змінено, 
+        або вона тимчасово недоступна.
+         Перевірте URL на помилки або поверніться на Головну</p>
+    </div>
+    <GoMain404/>
+ 
+    
+
+</div>
+
+
+</div>
+
+</section>
+  );
+};
